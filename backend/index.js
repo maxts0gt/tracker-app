6e39e203ec73a7578f1509d5d166dfb0dad69ce8
Max Tsogt <bazarsadtsogtjargal@gmail.com>
2022-07-16 22:03:40 +0900

    feat: fetch crypto currency data

--- backend/index.js
+++ backend/index.js
@@ -1,7 +1,8 @@
-const PORT = 3000;
-
+require("dotenv").config();
+const PORT = process.env.PORT;
 const express = require("express");
 const socketIO = require("socket.io");
+const axios = require("axios");
 
 const server = express().listen(PORT, () => {
   console.log(`Listening on port ${PORT}`);
@@ -21,3 +22,25 @@ socketHandler.on("connection", (socket) => {
   console.log("Client connected");
   socketHandler.emit("crypto", "Hello Users!");
 });
+
+const getPrices = () => {
+  axios
+    .get(process.env.LIST_URL)
+    .then((response) => {
+      const priceList = response.data.data.map((item) => {
+        return {
+          id: item.id,
+          name: item.symbol,
+          price: item.metrics.market_data.price_usd,
+        };
+      });
+      socketHandler.emit("crypto", priceList);
+    })
+    .catch((err) => {
+      console.log(err);
+    });
+};
+
+// setInterval(() => {
+//   getPrices();
+// }, 5000);
